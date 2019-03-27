858dc9ae4a2def57232632e7778bcf70d70d8a34
Jakub Grobelny <kubissus@gmail.com>
2019-03-27 11:15:29 +0100

    Prolog-L4Z3

--- 2018-2019/lato/Prolog/L4/zad3.pl
+++ 2018-2019/lato/Prolog/L4/zad3.pl
@@ -24,8 +24,7 @@ decimal(N, Digits, Acc) :-
     decimal(Div, Digits, [Digit | Acc]).
 
 split_input(Input, PartialResults, FinalResult) :-
-    length(FinalResult, 1),
-    append(PartialResults, FinalResult, Input),
+    append(PartialResults, [FinalResult], Input),
     !.
 
 gen_number([], []) :- 
@@ -37,7 +36,34 @@ gen_number([c | Xs], [N | Ns]) :-
     odd(N),
     gen_number(Xs, Ns).
 
-solve(Input) :-
+generate_partial_results(_, [], [], []) :-
+    !.
+generate_partial_results(Num, [Digit | Digits], [Template | Templates], [Result | Results]) :-
+    ResultVal is Num * Digit,
+    decimal(ResultVal, Result),
+    gen_number(Template, Result),
+    generate_partial_results(Num, Digits, Templates, Results).
+
+sum([], 0, _) :-
+    !.
+sum([N | Ns], Sum, Offset) :-
+    NextOffset is Offset * 10,
+    sum(Ns, SumPrev, NextOffset),
+    concat_number(N, NVal),
+    Sum is SumPrev + (NVal * Offset).
+
+solve(Input, Output) :-
     Input = [N1, N2 | Results],
     split_input(Results, PartialResults, FinalResult),
-    % ???
+    gen_number(N1, Num1),
+    gen_number(N2, Num2),
+    concat_number(Num1, Num1Val),
+    reverse(Num2, Num2Rev),
+    generate_partial_results(Num1Val, Num2Rev, PartialResults, Generated),
+    sum(Generated, Sum, 1),
+    decimal(Sum, SumDigits),
+    gen_number(FinalResult, SumDigits),
+    append([Num1], [Num2], Out0),
+    append(Out0, Generated, Out1),
+    append(Out1, [SumDigits], Output).
+
