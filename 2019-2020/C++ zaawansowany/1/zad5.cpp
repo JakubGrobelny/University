ac7e7ca03b5ce0bff53675c364eabe11b7c2e985
Jakub Grobelny <kubissus@gmail.com>
2019-10-09 23:29:18 +0200

    removed codewars solutions

--- 2019-2020/C++ zaawansowany/1/zad5.cpp
+++ 2019-2020/C++ zaawansowany/1/zad5.cpp
@@ -9,22 +9,24 @@ auto lucas_number(unsigned n) {
     return lucas_number(n-1) + lucas_number(n-2);
 }
 
-auto lucas_number_tail(unsigned n) {
- 
-    const auto aux = [](unsigned n, int l1, int l2, const auto& rec) {
-        if (n == 0)
-            return l1;
-        if (n == 1)
-            return l2;
-        return rec(n - 1, l2, l1 + l2, rec);
-    };
-
-    return aux(n, 2, 1, aux);
+auto lucas_number_tail(unsigned n, int l1, int l2) {
+    if (n == 0)
+        return l1;
+    if (n == 1)
+        return l2;
+    return lucas_number_tail(n - 1, l2, l1 + l2);
 }
 
 auto main() -> int {
     for (int i = 0; i < 10; i++)
-        std::cout << lucas_number(i) << std::endl;
+        std::cout << lucas_number(i) << ' ';
     
+    std::cout << std::endl;
+
+    for (int i = 0; i < 10; i++)
+        std::cout << lucas_number_tail(i, 2, 1) << ' ';
+
+    std::cout << std::endl;
+
     return 0;
 }
