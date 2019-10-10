025912f67b3559a3cf5b8a0d01d17410b553fb4f
Jakub Grobelny <kubissus@gmail.com>
2019-10-10 21:15:48 +0200

    C++ L1Z6 poprawione

--- 2019-2020/C++ zaawansowany/1/compile.sh
+++ 2019-2020/C++ zaawansowany/1/compile.sh
@@ -1,4 +1,4 @@
-# g++ zad1.cpp -o zad1.out --std=c++17 -Werror -Wall;
+g++ zad1.cpp -o zad1.out --std=c++17 -trigraphs; #-Werror -Wall ;
 g++ zad2.cpp -o zad2.out --std=c++17 -Werror -Wall;
 g++ zad3.cpp -o zad3.out --std=c++17 -Werror -Wall;
 g++ zad4.cpp -o zad4.out --std=c++17 -Werror -Wall;
--- 2019-2020/C++ zaawansowany/1/zad6.cpp
+++ 2019-2020/C++ zaawansowany/1/zad6.cpp
@@ -3,55 +3,44 @@
 #include <complex>
 
 
-void solve_quadratic_equation(int a, int b, int c) {
+void solve_quadratic_equation(double a, double b, double c) {
 
-    enum class ord {
-        greater, lesser, equal
+    auto approx_zero = [](double x) -> bool {
+        const float epsilon = 0.000001;
+        return x > -epsilon && x < epsilon;
     };
 
-    const auto cmp = [](int a, int b) -> ord {
-        if (a > b)
-            return ord::greater;
-        if (a < b)
-            return ord::lesser;
-        return ord::equal;
-    };
+    if (double delta = b*b - 4.0 * a * c; approx_zero(delta)) {
+        const double x = -b / (2.0 * a);
+        std::cout << "x = " << x << std::endl;
 
-    switch (auto delta = b*b - 4*a*c; cmp(delta, 0))
-    {
-        case ord::greater: {
-            double sqrt_delta = std::sqrt(delta);
-            double x0 = (-b + sqrt_delta) / (2 * a);
-            double x1 = (-b - sqrt_delta) / (2 * a);
-            std::cout << "x0 = " << x0 << std::endl 
-                      << "x1 = " << x1 << std::endl;
-            return;
-        }
-        case ord::lesser: {
-            using namespace std::complex_literals;
-            auto sqrt_delta = std::sqrt(-delta) * 1i / (2.0 * a);
-            auto x0 = std::complex<double>((-b/(2.0*a))) + sqrt_delta;
-            auto x1 = std::complex<double>((-b/(2.0*a))) - sqrt_delta;
-            std::cout << "x0 = " << x0 << std::endl 
-                      << "x1 = " << x1 << std::endl;
-            return;
-        }
-        default: {
-            auto x0 = -b / (2*a);
-            std::cout << "x0 = " << x0 << std::endl;
-            return;
-        }
+    } else if (delta > 0) {
+        const double sqrt_delta = std::sqrt(delta);
+        const double x0 = (-b + sqrt_delta) / (2*a);
+        const double x1 = c / a * x0;
+    
+        std::cout << "x0 = " << x0 << std::endl << "x1 = " << x1 << std::endl;
+    } else {
+        using namespace std::complex_literals;
+        const std::complex<double> im = std::sqrt(-delta) * 1i / (2.0*a);
+        const double real = -b / (2 * a);
+
+        const std::complex<double> x0 = real + im;
+        const std::complex<double> x1 = real - im;
+        
+        std::cout << "x0 = " << x0 << std::endl << "x1 = " << x1 << std::endl;
     }
 }
 
 auto main() -> int {
+
     std::cout << "x^2 - 2x:" << std::endl;
     solve_quadratic_equation(2, -2, 0);
 
     std::cout << std::endl;
 
-    std::cout << "5x^2:" << std::endl;
-    solve_quadratic_equation(5, 0, 0);
+    std::cout << "x^2 - 2x + 1:" << std::endl;
+    solve_quadratic_equation(1, -2, 1);
 
     std::cout << std::endl;
 
