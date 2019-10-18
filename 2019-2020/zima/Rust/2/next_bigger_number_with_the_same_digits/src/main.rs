35c0a9d50cbcd114878bdcb2bc7b62953c44ec22
Jakub Grobelny <kubissus@gmail.com>
2019-10-18 19:16:09 +0200

    Rust: L2 - next_bigger_number

--- 2019-2020/zima/Rust/2/next_bigger_number_with_the_same_digits/src/main.rs
+++ 2019-2020/zima/Rust/2/next_bigger_number_with_the_same_digits/src/main.rs
@@ -1,27 +1,96 @@
 fn next_bigger_number(n: i64) -> i64 {
-    let mut characters : Vec<char> = n.to_string().chars().collect();
+    let mut digits : Vec<char> = n.to_string().chars().collect();
 
-    fn is_sorted(vec: Vec<char>, cmp: &dyn Fn(char, char) -> bool) -> bool {
-        for i in 1..vec.len() {
-            if !cmp(vec[i], vec[i-1]) { return false }
+    fn is_sorted_desc(digits: &Vec<char>) -> bool {
+        for i in 1..digits.len() {
+            if digits[i-1] < digits[i] { return false };
         }
         true
     }
 
-    if is_sorted(characters, &|a, b| a <= b) || n <= 9 { 
-        -1 
-    } else if is_sorted(characters, &|a, b| a >= b) {
-        let temp = characters[characters.len()-1];
-        characters[characters.len()-1] = characters[characters.len()-2];
-        characters[characters.len()-2] = temp;
-        characters.into_iter().collect::<String>().parse::<i64>().unwrap_or(-1)
-    } else {
-        -1
+    fn rightmost_smaller(digits: &Vec<char>) -> usize {
+        for i in (0..digits.len()-1).rev() {
+            if digits[i] < digits[i+1] {
+                return i;
+            }
+        }
+        0
     }
 
+    if n < 10 || is_sorted_desc(&digits) { 
+        -1 
+    } else {        
+        let first_smaller = rightmost_smaller(&digits);
+        let mut first_greater = first_smaller+1;
+        
+        for i in first_smaller+1..digits.len() {
+            if digits[i] > digits[first_smaller] && 
+               digits[first_greater] > digits[i] {
+                first_greater = i;
+            }
+        }
+
+        let temp = digits[first_smaller];
+        digits[first_smaller] = digits[first_greater];
+        digits[first_greater] = temp;
+
+        let mut digits_last : Vec<char> = digits[first_smaller+1..].to_vec();
+        digits_last.sort();
+                
+        for i in first_smaller+1..digits.len() {
+            digits[i] = digits_last[i - first_smaller - 1];
+        }
+        digits.iter().collect::<String>().parse::<i64>().unwrap_or(-1)
+    }
 }
+
 
 #[test]
 fn test0() {
     assert_eq!(next_bigger_number(0), -1)
+}
+
+#[test]
+fn test1() {
+    assert_eq!(next_bigger_number(1234), 1243)
+}
+
+#[test]
+fn test2() {
+    assert_eq!(next_bigger_number(4321), -1)
+}
+
+#[test]
+fn test3() {
+    assert_eq!(next_bigger_number(21), -1)
+}
+
+#[test]
+fn test4() {
+    assert_eq!(next_bigger_number(12), 21)
+}
+
+#[test]
+fn test5() {
+    assert_eq!(next_bigger_number(5823), 5832)
+}
+
+#[test]
+fn test6() {
+    assert_eq!(next_bigger_number(9989), 9998)
+}
+
+#[test]
+fn test7() {
+    assert_eq!(next_bigger_number(10_000_001), 10_000_010)
+}
+
+#[test]
+fn test8() {
+    assert_eq!(next_bigger_number(144), 414);
+}
+
+#[test]
+fn test9() {
+    assert_eq!(next_bigger_number(9899), 9989)
 }
