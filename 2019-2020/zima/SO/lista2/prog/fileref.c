25937d6100b30d03ee4924b6fecad80b92ce9766
Jakub Grobelny <kubissus@gmail.com>
2019-10-22 00:37:44 +0200

    SO l2z6

--- 2019-2020/zima/SO/lista2/prog/fileref.c
+++ 2019-2020/zima/SO/lista2/prog/fileref.c
@@ -6,25 +6,67 @@ static char buf[256];
 #define LINE2 33
 #define LINE3 78
 
-static void do_read(int fd) {
+static void do_read(int fd) 
+{
+    Fork();
+    pid_t pid = getpid();
 
-  exit(0);
+    const int chars_to_read = 16;
+
+    const off_t prev_offset = Lseek(fd, 0, SEEK_CUR);
+    printf("[%d]: %ld\n", pid, prev_offset);
+
+    if (Read(fd, buf, chars_to_read) != chars_to_read) 
+        app_error("Failed to read the file!");
+
+    const off_t end_offset = Lseek(fd, 0, SEEK_CUR);
+    printf("[%d]: %ld\n", pid, end_offset);
+
+    if (pid) 
+    {
+        Waitpid(pid, NULL, WUNTRACED);
+        Close(fd);
+    }
+
+    exit(0);
 }
 
-static void do_close(int fd) {
+static void do_close(int fd) 
+{
+    if (Fork()) 
+    {
+        Close(fd);
+        printf("[%d]: closed the file.\n", getpid());
+        Waitpid(-1, NULL, WUNTRACED);
+        exit(0);
+    }
+    else
+    {
+        pid_t pid = getpid();
 
-  exit(0);
+        sleep(1);
+
+        if (Read(fd, buf, LINE1) != LINE1)
+            app_error("[%d]: Failed to read from file!", pid);
+        
+        buf[LINE1] = '\0';
+        printf("[%d]: %s", pid, buf);
+
+        exit(0);
+    }
 }
 
-int main(int argc, char **argv) {
-  if (argc != 2)
-    app_error("Usage: %s [read|close]", argv[0]);
+int main(int argc, char **argv) 
+{
+    if (argc != 2)
+        app_error("Usage: %s [read|close]", argv[0]);
 
-  int fd = Open("test.txt", O_RDONLY, 0);
+    int fd = Open("test.txt", O_RDONLY, 0);
 
-  if (!strcmp(argv[1], "read"))
-    do_read(fd);
-  if (!strcmp(argv[1], "close"))
-    do_close(fd);
-  app_error("Unknown variant '%s'", argv[1]);
+    if (!strcmp(argv[1], "read"))
+        do_read(fd);
+    if (!strcmp(argv[1], "close"))
+        do_close(fd);
+
+    app_error("Unknown variant '%s'", argv[1]);
 }
