a532fe1d9f1bdf0c003fe8b639ae3544a035cdab
Jakub Grobelny <kubissus@gmail.com>
2019-10-22 01:12:46 +0200

    SO l2z7 unfinished/broken

--- 2019-2020/zima/SO/lista2/prog/het.c
+++ 2019-2020/zima/SO/lista2/prog/het.c
@@ -1,48 +1,70 @@
 #include "csapp.h"
 
-static int ndselect(int n) {
-  /* TODO: the body of this procedure is missing! */
-  exit(0);
+
+static int conflict(int x1, int y1, int x2, int y2) 
+{
+    return x1 == x2
+        || y1 == y2
+        || x1 + y1 == x2 + y2
+        || x1 - y1 == x2 - y2;
 }
 
-static int conflict(int x1, int y1, int x2, int y2) {
-  return x1 == x2
-    || y1 == y2
-    || x1 + y1 == x2 + y2
-    || x1 - y1 == x2 - y2;
+static void print_line_sep(int size) 
+{
+    for (int i = 0; i < size; ++i) 
+        printf("+---");
+    printf("+\n");
 }
 
-static void print_line_sep(int size) {
-  for (int i = 0; i < size; ++i) 
-    printf("+---");
-  printf("+\n");
-}
-
-static void print_board(int size, int board[size]) {
-  for (int i = 0; i < size; ++i) {
+static void print_board(int size, int board[size]) 
+{
+    for (int i = 0; i < size; ++i) 
+    {
+        print_line_sep(size);
+        for (int j = 0; j < size; ++j)
+            printf("|%s", board[i] == j ? " Q " : "     ");
+        printf("|\n");
+    }
     print_line_sep(size);
-    for (int j = 0; j < size; ++j)
-      printf("|%s", board[i] == j ? " Q " : "   ");
-    printf("|\n");
-  }
-  print_line_sep(size);
-  printf("\n");
+    printf("\n");
 }
 
-int main(int argc, char **argv) {
-  if (argc != 2)
-    app_error("Usage: %s [SIZE]", argv[0]);
+static int ndselect(int n) 
+{
+    if (n == 0)
+        return 0;
+    else if (Fork())
+        return ndselect(n-1);
+    else
+        return n;
+}
 
-  int size = atoi(argv[1]);
+int main(int argc, char **argv) 
+{
+    if (argc != 2)
+        app_error("Usage: %s [SIZE]", argv[0]);
 
-  if (size < 3 || size > 9)
-    app_error("Give board size in range from 4 to 9!");
+    const int size = atoi(argv[1]);
 
-  int board[size];
+    if (size < 3 || size > 9)
+        app_error("Give board size in range from 4 to 9!");
 
-  /* TODO: a loop is missing here! */
+    int board[size];
 
-  print_board(size, board);
+    for (int i = 0; i < size; i++) 
+    {
+        board[i] = ndselect(size);
 
-  return 0;
+        for (int prev = 0; prev < i; prev++) 
+        {
+            if (conflict(i, board[i], prev, board[prev]))
+            {
+                exit(0);
+            }
+        }
+    }
+
+    print_board(size, board);
+
+    return 0;
 }
