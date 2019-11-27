8114e56e02054fbfc1f9cf034d378fc78ca47a2c
Jakub Grobelny <kubissus@gmail.com>
2019-11-27 17:14:00 +0100

    PiZZO Z2 dodano konwersję na graf

--- 2019-2020/zima/PiZZO/zadanie2/main.rkt
+++ 2019-2020/zima/PiZZO/zadanie2/main.rkt
@@ -2,22 +2,53 @@
 #lang racket
 
 (require json)
+(require racket/set)
 
+;;; converts the JSON problem instance into a graph
 (define (to-graph problem)
-    ; TODO: implement
-    problem)
+    ;;; new graph
+    (define empty-graph 
+        (make-hasheq '()))
+    ;;; destructively adds edges <v1, v2> and <v2,v1> to the graph
+    (define (add-edge graph v1 v2)
+        ;;; get the set of neighbours of the given vertex. Create the set if
+        ;;; needed
+        (define (get-neighbours vertex)
+            (if (hash-has-key? graph vertex)
+                (hash-ref graph vertex)
+                (begin
+                    (hash-set! graph vertex (mutable-seteq))
+                    (hash-ref graph vertex))))
+        (let ([v1-neighbours (get-neighbours v1)]
+              [v2-neighbours (get-neighbours v2)])
+                (set-add! v1-neighbours v2)
+                (set-add! v2-neighbours v1)))
+    ;;; converts list of conflicts into a graph
+    (define (conflicts-to-graph conflicts graph)
+        (match conflicts
+            ['() graph]
+            [(cons conflict conflicts)
+                (let* ([from (hash-ref conflict 'zrzeda)]
+                       [to (hash-ref conflict 'nielubiany)])
+                    (add-edge graph from to)
+                    (conflicts-to-graph conflicts graph))]))
+    (conflicts-to-graph (hash-ref problem 'konflikty) empty-graph))
 
+
+;;; reduces the size of the graph by removing uneccessary vertices
 (define (reduce-graph graph)
     ; TODO: implement
     graph)
 
+;;; converts the graph into SAT problem instance
 (define (to-sat graph)
     ;TODO: implement
     graph)
 
+;;; prints the SAT instance in the correct format
 (define (print-sat sat)
     ;TODO: implement
-    (fprintf (current-error-port) "~a" sat))
+    (fprintf (current-output-port) "~a" sat))
 
 (module+ main
     (let* ([problem (read-json)]
