7e67842a84296ac800ee451406912df8199443de
Jakub Grobelny <jgrobelny1@gmail.com>
2019-11-28 23:29:16 +0100

    C++ L6

--- 2019-2020/zima/C++ zaawansowany/L6/zad1.cpp
+++ 2019-2020/zima/C++ zaawansowany/L6/zad1.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
+#include <algorithm>
+#include <random>
+#include <vector>
 
 
-auto main(void) -> int {
-    std::cout << "Hello, world!" << std::endl;
+auto main(int argc, char* argv[]) -> int {
+
+    std::vector<char*> strings;
+
+    if (argc < 2) {
+        std::cerr << "Error: no input!" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    for (int i = 1; i < argc; i++) {
+        strings.push_back(argv[i]);
+    }
+
+    std::mt19937 gen {std::random_device{}()};
+
+    std::shuffle(strings.begin(), strings.end(), gen);
+
+    for (auto& str : strings) {
+        std::cout << str << ' ';
+    }
+    std::cout << std::endl;
+
     return EXIT_SUCCESS;
 }
--- 2019-2020/zima/C++ zaawansowany/L6/zad2.cpp
+++ 2019-2020/zima/C++ zaawansowany/L6/zad2.cpp
@@ -1,7 +1,39 @@
 #include <iostream>
+#include <random>
+#include <fstream>
 
 
-auto main(void) -> int {
-    std::cout << "Hello, world!" << std::endl;
+auto main(int argc, char* argv[]) -> int {
+
+    if (argc != 4) {
+        std::cerr << "Invalid number of files specified (expecting 3)!" 
+                  << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    std::mt19937 gen {std::random_device{}()};
+
+    constexpr int count = 1000;
+
+    std::ofstream uniform(argv[1]);
+    std::ofstream binomial(argv[2]);
+    std::ofstream normal(argv[3]);  
+
+    auto uniform_distr = std::uniform_real_distribution<double>{0, 100};
+    auto binomial_distr = std::binomial_distribution<int>{100, 0.5};
+    auto normal_distr = std::normal_distribution<double>{50, 5};
+
+    for (int i = 0; i < count; i++) {
+        uniform << uniform_distr(gen) << std::endl;
+    }
+
+    for (int i = 0; i < count; i++) {
+        binomial << binomial_distr(gen) << std::endl;
+    }
+
+    for (int i = 0; i < count; i++) {
+        normal << normal_distr(gen) << std::endl;
+    }
+
     return EXIT_SUCCESS;
 }
