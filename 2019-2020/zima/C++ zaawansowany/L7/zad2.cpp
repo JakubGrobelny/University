6d06680ac6a2bef95a0dd4f7ec1e33576f13a7dd
Jakub Grobelny <jgrobelny1@gmail.com>
2019-12-06 00:16:49 +0100

    C++ L7Z2

--- 2019-2020/zima/C++ zaawansowany/L7/zad2.cpp
+++ 2019-2020/zima/C++ zaawansowany/L7/zad2.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
+#include <complex>
+
+
+auto dzeta(std::complex<double> z, uint64_t iters) -> std::complex<double> {
+    std::complex<double> result = 0.0;
+    for (uint64_t n = 1; n <= iters; n++){
+        result += 1.0 / std::pow(n, z);
+    }
+    return result;
+}
 
 
 auto main() -> int {
-    std::cout << "Hello, world!" << std::endl;
+    uint64_t scale;
+    std::cin >> scale;
+
+    uint64_t iterations;
+    std::cin >> iterations;
+
+    double range_start;
+    double range_end;
+
+    std::cin >> range_start >> range_end;
+
+    auto dzeta_critical = [&](double imag) {
+        return dzeta({0.5, imag}, iterations);
+    };
+
+    const double step = 1.0 / scale;
+    for (double point = range_start; point <= range_end; point += step) {
+        const auto value = dzeta_critical(point);
+        std::cout << point        << ", " 
+                  << value.real() << ", "
+                  << value.imag() << std::endl;
+    }
 }
