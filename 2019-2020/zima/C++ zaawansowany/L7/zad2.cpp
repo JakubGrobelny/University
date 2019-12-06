474120c7882614b751ac4de5ee9cde5fd48ea552
Jakub Grobelny <kubissus@gmail.com>
2019-12-06 10:43:20 +0100

    C++ L7 poprawki

--- 2019-2020/zima/C++ zaawansowany/L7/zad2.cpp
+++ 2019-2020/zima/C++ zaawansowany/L7/zad2.cpp
@@ -4,10 +4,20 @@
 
 auto dzeta(std::complex<double> z, uint64_t iters) -> std::complex<double> {
     std::complex<double> result = 0.0;
-    for (uint64_t n = 1; n <= iters; n++){
+    for (uint64_t n = 1; n <= iters; n++) {
         result += 1.0 / std::pow(n, z);
     }
     return result;
+}
+
+
+auto dzeta2(std::complex<double> z, uint64_t iters) -> std::complex<double> {
+    std::complex<double> result = 0.0;
+    for (uint64_t n = 1; n <= iters; n++) {
+        auto nf = static_cast<double>(n);
+        result += std::pow(-1.0, nf+1.0) / std::pow(nf, z);
+    }
+    return result / (1.0 - 2.0/std::pow(2.0, z));
 }
 
 
@@ -24,7 +34,7 @@ auto main() -> int {
     std::cin >> range_start >> range_end;
 
     auto dzeta_critical = [&](double imag) {
-        return dzeta({0.5, imag}, iterations);
+        return dzeta2({0.5, imag}, iterations);
     };
 
     const double step = 1.0 / scale;
