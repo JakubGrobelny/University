1431c5a516f7a5f01118c0f7792a150b620edbd3
Jakub Grobelny <kubissus@gmail.com>
2020-01-01 21:19:29 +0100

    PiZZO Z3 więcej

--- 2019-2020/zima/PiZZO/zadanie3/main.py
+++ 2019-2020/zima/PiZZO/zadanie3/main.py
@@ -1,10 +1,31 @@
 #! /usr/bin/python3
-from typing import *
-from z3     import *
+from typing import Dict, List, Union, Optional
+import z3
 import json
 
 
-def solve(instance: dict) -> Optional[dict]:
+class Ingredient:
+    def __init__(self, properties: Dict[str, Union[float, str]]):
+        self.name : str = properties['nazwa']
+        del properties['nazwa']
+        self.macros : Dict[str, float] = properties
+
+class Instance:
+    def __init__(self, filename: str):
+        with open(filename) as file:
+            instance = json.loads(file.read().replace('\n', ''))
+        self.macros : List[str] = instance['parametry']
+        self.ingredients = []
+        for ingredient in instance['składniki']:
+            self.ingredients.append(Ingredient(ingredient))
+        self.conflicts = set()
+        for conflict in instance['konflikty']:
+            first, second = conflict['nazwa1'], conflict['nazwa2']
+            self.conflicts.add((first, second))
+            self.conflicts.add((second, first))
+        self.target = instance['cel']
+
+def solve(instance: Instance) -> Optional[dict]:
     return None
 
 def print_diet(diet: dict):
@@ -12,8 +33,7 @@ def print_diet(diet: dict):
 
 if __name__ == "__main__":
     filename = input()
-    with open(filename) as file:
-        instance = json.loads(file.read().replace('\n', ''))
+    instance = Instance(filename)
     result = solve(instance)
     if result is None:
         print("Nie można wygenerować diety.")
