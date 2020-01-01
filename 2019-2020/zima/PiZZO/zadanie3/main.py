1d7c16cc63598656a7453f7820af4e0723485c9c
Jakub Grobelny <kubissus@gmail.com>
2020-01-01 22:11:36 +0100

    PiZZO Z3 więcej

--- 2019-2020/zima/PiZZO/zadanie3/main.py
+++ 2019-2020/zima/PiZZO/zadanie3/main.py
@@ -1,5 +1,5 @@
 #! /usr/bin/python3
-from typing import Dict, List, Union, Optional
+from typing import *
 import z3
 import json
 
@@ -10,29 +10,40 @@ class Ingredient:
         del properties['nazwa']
         self.macros : Dict[str, float] = properties
 
+class Target:
+    def __init__(self, macro: str, targets: Dict[str, float]):
+        self.macro : str = macro
+        self.min : float = targets['min']
+        self.max : float = targets['max']
+
 class Instance:
     def __init__(self, filename: str):
         with open(filename) as file:
             instance = json.loads(file.read().replace('\n', ''))
         self.macros : List[str] = instance['parametry']
-        self.ingredients = []
+        self.ingredients : List[Ingredient] = []
         for ingredient in instance['składniki']:
             self.ingredients.append(Ingredient(ingredient))
-        self.conflicts = set()
+        self.conflicts : Set[Tuple[str, str]] = set()
         for conflict in instance['konflikty']:
             first, second = conflict['nazwa1'], conflict['nazwa2']
             self.conflicts.add((first, second))
             self.conflicts.add((second, first))
-        self.target = instance['cel']
+        self.targets : List[Target] = []
+        raw_targets = instance['cel']
+        for macro in raw_targets.keys():
+            self.targets.append(Target(macro, raw_targets[macro]))
 
 def solve(instance: Instance) -> Optional[dict]:
-    return None
-
-def print_diet(diet: dict):
+    # TODO
     pass
 
+def print_diet(diet: Dict[str, List[str]]):
+    for meal in diet.keys():
+        print(meal + ', '.join(diet[meal]))
+
 if __name__ == "__main__":
-    filename = input()
+    filename : str = input()
     instance = Instance(filename)
     result = solve(instance)
     if result is None:
