599796898040fcdb97d34adc1b8daa49670e9595
Jakub Grobelny <kubissus@gmail.com>
2020-01-02 01:58:00 +0100

    PiZZO Z3 prawie gotowe

--- 2019-2020/zima/PiZZO/zadanie3/main.py
+++ 2019-2020/zima/PiZZO/zadanie3/main.py
@@ -48,28 +48,33 @@ def optimize(instance: Instance):
 def make_variable_name(meal: int, ingredient: int) -> str:
     return 'var_' + str(ingredient) + '/' + str(meal)
 
-def prepare_solver(instance: Instance) -> Tuple[z3.Solver, Dict[str, int]]:
+def split_variable_name(var: str) -> Tuple[int,int]:
+    return tuple(map(int, var.split('_')[1].split('/')))
+
+def prepare_solver(instance: Instance) -> Tuple[z3.Solver, Dict[int, str]]:
     solver = z3.Solver()
     variables = []
     ingredient_indices : Dict[str, int] = {}
+    ingredient_names : Dict[int, str] = {}
     num_of_meals = len(MEAL_NAMES)
     # create a list of variables for each ingredient
-    for ingredient_index in range(len(instance.ingredients)):
+    for ingredient_id in range(len(instance.ingredients)):
         meal_vars = []
-        name = instance.ingredients[ingredient_index].name
-        ingredient_indices[name] = ingredient_index
-        for meal_index in range(num_of_meals):
-            var_name = make_variable_name(meal_index, ingredient_index)
+        name = instance.ingredients[ingredient_id].name
+        ingredient_indices[name] = ingredient_id
+        ingredient_names[ingredient_id] = name
+        for meal_id in range(num_of_meals):
+            var_name = make_variable_name(meal_id, ingredient_id)
             var = z3.Int(var_name)
             solver.add(var >= 0)
             meal_vars.append(z3.ToReal(var))
         variables.append(meal_vars)
     # add constraints for food conflicts
     for food1, food2 in instance.conflicts:
-        food1_index = ingredient_indices[food1]
-        food2_index = ingredient_indices[food2]
-        food1_vars = variables[food1_index]
-        food2_vars = variables[food2_index]
+        food1_id = ingredient_indices[food1]
+        food2_id = ingredient_indices[food2]
+        food1_vars = variables[food1_id]
+        food2_vars = variables[food2_id]
         for var1, var2 in zip(food1_vars, food2_vars):
             solver.add(z3.Or(var1 == 0, var2 == 0))
     # add constraints for total amount of substances
@@ -84,12 +89,27 @@ def prepare_solver(instance: Instance) -> Tuple[z3.Solver, Dict[str, int]]:
         macro_sum = macro_sums[target.macro]
         solver.add(macro_sum >= target.min)
         solver.add(macro_sum <= target.max)
-    return solver, ingredient_indices
+    # ensure that meals are not empty
+    num_of_ingredients = len(variables)
+    for meal_id in range(num_of_meals):
+        meal_conditions = []
+        for ingredient_id in range(num_of_ingredients):
+            meal_conditions.append(variables[ingredient_id][meal_id] > 0)
+        solver.add(z3.Or(meal_conditions))
 
-def unpack_diet(result: z3.Model, food: Dict[str, int]) -> Dict[str, List[str]]:
-    # TODO:
-    return {}
+    return solver, ingredient_names
 
+def unpack_diet(result: z3.Model, food: Dict[int, str]) -> Dict[str, List[str]]:
+    diet =  {}
+    for meal in MEAL_NAMES:
+        diet[meal] = []
+    for decl in result:
+        var = decl.name()
+        ingredient_id, meal_id = split_variable_name(var)
+        occurences = result[decl].as_long()
+        if occurences > 0:
+            diet[MEAL_NAMES[meal_id]] += [food[ingredient_id]] * occurences
+    return diet
 
 def solve(instance: Instance) -> Optional[Dict[str, List[str]]]:
     optimize(instance)
@@ -100,7 +120,7 @@ def solve(instance: Instance) -> Optional[Dict[str, List[str]]]:
 
 def print_diet(diet: Dict[str, List[str]]):
     for meal in diet.keys():
-        print(meal + ', '.join(diet[meal]))
+        print(meal + ': ' + ', '.join(diet[meal]))
 
 if __name__ == "__main__":
     filename : str = input()
