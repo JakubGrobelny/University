8e8b30bae61461da529f042c123d1837f7c6b0fd
Jakub Grobelny <jgrobelny1@gmail.com>
2020-01-09 01:48:18 +0100

    C++ L9Z2

--- 2019-2020/zima/C++ zaawansowany/L9/zad1.cpp
+++ 2019-2020/zima/C++ zaawansowany/L9/zad1.cpp
@@ -13,11 +13,10 @@ auto main() -> int {
     std::vector<float> numbers(std::istream_iterator<float>(stream), {});
 
     std::cout << std::setprecision(3) << std::fixed;
-
     std::ostream_iterator<float> out(std::cout, " ");
 
     std::for_each(numbers.rbegin(), numbers.rend(), [&](float x) {
-        *out = x;
+        out = x;
     });
 
     std::cout << std::endl;
--- 2019-2020/zima/C++ zaawansowany/L9/zad2.cpp
+++ 2019-2020/zima/C++ zaawansowany/L9/zad2.cpp
@@ -1,5 +1,52 @@
+#include <iostream>
+#include <iterator>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+#include <fstream>
+
+
+auto gcd(uint64_t a, uint64_t b) -> uint64_t {
+    if (!b) {
+        return a;
+    } else {
+        return gcd(b, a % b);
+    }
+}
+
+
+auto totient(uint64_t k) -> uint64_t {
+    uint64_t result = 0;
+
+    for (uint64_t i = 1; i <= k; i++) {
+        if (gcd(k, i) == 1) {
+            result++;
+        }
+    }
+
+    return result;
+}
+
+
+struct generator {
+    int k = 1;
+    int operator()() {
+        return totient(this->k++);
+    }
+};
 
 
 auto main() -> int {
+    int k;
+    std::cin >> k;
+    
+    std::vector<uint64_t> values(k);
+    std::generate(values.begin(), values.end(), generator());
 
+    std::ofstream file("phi.txt");
+    std::ostream_iterator<uint64_t> out(file, "; ");
+
+    std::for_each(values.rbegin(), values.rend(), [&](uint64_t n) {
+        out = n;
+    });
 }
