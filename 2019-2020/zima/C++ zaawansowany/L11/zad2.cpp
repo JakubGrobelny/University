451d594fe8058b27fe60bea45692ff05d966a70b
Jakub Grobelny <jgrobelny1@gmail.com>
2020-01-17 00:50:17 +0100

    C++ L11Z2

--- 2019-2020/zima/C++ zaawansowany/L11/zad2.cpp
+++ 2019-2020/zima/C++ zaawansowany/L11/zad2.cpp
@@ -1,3 +1,34 @@
+#include <iostream>
+
+
+template <int N, int K>
+class binomial_coefficient {
+public:
+    constexpr auto operator() () const -> int {
+        return binomial_coefficient<N-1,K-1>()() * N / K;
+    }
+};
+
+template <int N>
+class binomial_coefficient<N,N> {
+public:
+    constexpr auto operator() () const -> int {
+        return 1;
+    }
+};
+
+
+template <int N>
+class binomial_coefficient<N,0> {
+public:
+    constexpr auto operator() () const -> int {
+        return 1;
+    }
+};
+
+
+
 auto main(int argc, char* argv[]) -> int {
-    
+    constexpr int coeff = binomial_coefficient<17,9>()();
+    std::cout << coeff << std::endl;
 }
