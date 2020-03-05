ab13bb45f1eadd2495e63aab878953ffcbcbb978
Jakub Grobelny <jgrobelny1@gmail.com>
2020-03-05 03:14:59 +0100

    Haskell L1Z2

--- 2019-2020/lato/Haskell/L1/Jakub_Grobelny_1.hs
+++ 2019-2020/lato/Haskell/L1/Jakub_Grobelny_1.hs
@@ -2,9 +2,9 @@
 -- Kurs języka Haskell
 -- Lista 10.03.2020
 
--- Zadanie 1
 import Prelude hiding (concat, and, all, maximum)
 
+-- Zadanie 1
 intercalate :: [a] -> [[a]] -> [a]
 intercalate _ []  = []
 intercalate sep (xs:xss) = xs ++ (xss >>= (sep ++))
@@ -32,3 +32,28 @@ all pred = foldr ((&&) . pred) True
 maximum :: [Integer] -> Integer
 maximum [] = undefined
 maximum (x:xs) = foldr max x xs
+
+-- Zadanie 2
+newtype Vector a = Vector { fromVector :: [a] } deriving Show
+
+scaleV :: Num a => a -> Vector a -> Vector a
+scaleV scalar = Vector . map (scalar *) . fromVector
+
+norm :: Floating a => Vector a -> a
+norm = sqrt . sum . fromVector
+
+scalarProd :: Num a => Vector a -> Vector a -> a
+scalarProd u v
+    | length ux /= length vx = error "Vector length mismatch!"
+    | otherwise = sum $ zipWith (*) ux vx
+  where
+    ux = fromVector u
+    vx = fromVector v
+
+sumV :: Num a => Vector a -> Vector a -> Vector a
+sumV u v
+    | length ux /= length vx = error "Vector length mismatch!"
+    | otherwise = Vector $ zipWith (+) ux vx
+  where
+    ux = fromVector u
+    vx = fromVector v
