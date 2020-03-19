b78aaac665b819500323719c7fde8ec0548f02cc
Jakub Grobelny <jgrobelny1@gmail.com>
2020-03-19 23:27:51 +0100

    Haskell L2Z8

--- 2019-2020/lato/Haskell/L2/Jakub_Grobelny_2.hs
+++ 2019-2020/lato/Haskell/L2/Jakub_Grobelny_2.hs
@@ -54,9 +54,9 @@ ipermP = undefined
 spermP :: [a] -> [[a]]
 spermP = undefined
 
--- uwaga: działa tylko dla list o równej długości, tak jak zipF z listy zadań
+-- uwaga: działa tylko dla list o równej długości (tak jak zipF z listy zadań)
 zipP :: [a] -> [b] -> [(a,b)]
-zipP = join . join $ bool zipOnce (const $ const []) .: bothNull
+zipP = (join . join) $ bool zipOnce (const $ const []) .: bothNull
   where
     bothNull = uncurry (&&) .: curry (null *** null)
     heads = (head *** head)
@@ -112,6 +112,7 @@ insertBST a tree@(NodeBST left val right)
     | otherwise = NodeBST left val (insertBST a right)
 
 -- Zadanie 6
+
 deleteMaxBST :: Ord a => BST a -> (BST a, a)
 deleteMaxBST EmptyBST = error "deleteMaxBST: empty tree!"
 deleteMaxBST (NodeBST left val EmptyBST) = (left, val)
@@ -146,3 +147,61 @@ search23 a (Node3 left val1 mid val2 right)
     | a < val1  = search23 a left
     | a < val2  = search23 a mid
     | otherwise = search23 a right
+
+-- Zadanie 8
+
+data InsertResult a 
+    = Balanced (Tree23 a)
+    | Grown    (Tree23 a) a (Tree23 a)
+
+insert23 :: Ord a => a -> Tree23 a -> Tree23 a
+insert23 a tree = case insert' a tree of
+    Balanced tree' -> tree'
+    Grown l v r    -> Node2 l v r
+
+insert' :: Ord a => a -> Tree23 a -> InsertResult a
+insert' x Empty23 = Grown Empty23 x Empty23
+insert' x tree@(Node2 left val right)
+    | x < val   = case insert' x left of
+        Balanced left'          -> Balanced $ Node2 left' val right
+        Grown lleft lval lright -> Balanced $ Node3 lleft lval lright val right
+    | x > val   = case insert' x right of
+        Balanced right'         -> Balanced $ Node2 left val right'
+        Grown rleft rval rright -> Balanced $ Node3 left val rleft rval rright
+    | otherwise = Balanced tree
+insert' x tree@(Node3 left a1 mid a2 right)
+    | x <  a1 = case insert' x left of
+        Balanced left'          -> Balanced $ Node3 left' a1 mid a2 right
+        Grown lleft lval lright -> 
+            Grown (Node2 lleft lval lright) a1 (Node2 mid a2 right)
+    | x == a1 = Balanced tree
+    | x <  a2 = case insert' x mid of
+        Balanced mid'           -> Balanced $ Node3 left a1 mid' a2 right
+        Grown mleft mval mright ->
+            Grown (Node2 left a1 mleft) mval (Node2 mright a2 right)
+    | x == a2 = Balanced tree
+    | otherwise = case insert' x right of
+        Balanced right'         -> Balanced $ Node3 left a1 mid a2 right'
+        Grown rleft rval rright ->
+            Grown (Node2 left a1 mid) a2 (Node2 rleft rval rright)
+
+-- Pomocnicze funkcje do testowania 2-3-drzew
+listTo23Tree :: Ord a => [a] -> Tree23 a
+listTo23Tree [] = Empty23
+listTo23Tree (x:xs) = insert23 x $! listTo23Tree xs
+
+pathLengths :: Tree23 a -> Maybe Int
+pathLengths Empty23 = Just 0
+pathLengths (Node2 l _ r) = do
+    llen <- pathLengths l
+    rlen <- pathLengths r
+    if llen == rlen
+        then return $! llen + 1
+        else Nothing
+pathLengths (Node3 l _ m _ r) = do
+    llen <- pathLengths l
+    mlen <- pathLengths m
+    rlen <- pathLengths r
+    if llen == mlen && mlen == rlen
+        then return $! llen + 1
+        else Nothing
