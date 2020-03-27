d42e95417ca7aaf61570d414fe3519fbe8bb14cb
Jakub Grobelny <jgrobelny1@gmail.com>
2020-03-27 14:10:25 +0100

    Haskell L3Z6

--- 2019-2020/lato/Haskell/L3/Jakub_Grobelny_3.hs
+++ 2019-2020/lato/Haskell/L3/Jakub_Grobelny_3.hs
@@ -4,6 +4,8 @@
 
 {-# LANGUAGE ViewPatterns #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE IncoherentInstances #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 data BTree t a
     = Node (t a) a (t a)
@@ -25,6 +27,8 @@ newtype Unbalanced a = Unbalanced { fromUnbalanced :: BTree Unbalanced a }
 instance BT Unbalanced where
     toTree = fromUnbalanced
 
+--------------------------------------------------------------------------------
+
 -- Zadanie 1
 
 treeSize :: BT t => t a -> Int
@@ -38,6 +42,8 @@ treeLabels (toTree -> Node l a r) = treeLabels l ++ [a] ++ treeLabels r
 treeFold :: BT t => (b -> a -> b -> b) -> b -> t a -> b
 treeFold _ n (toTree -> Leaf) = n
 treeFold f n (toTree -> Node l a r) = f (treeFold f n l) a (treeFold f n r)
+
+--------------------------------------------------------------------------------
 
 -- Zadanie 2
 
@@ -57,6 +63,8 @@ toUnbalanced (toTree -> Leaf) = Unbalanced Leaf
 toUnbalanced (toTree -> Node l a r) = Unbalanced $ 
     Node (toUnbalanced l) a (toUnbalanced r)
 
+--------------------------------------------------------------------------------
+
 -- Zadanie 3
 
 instance (BT t, Show a) => Show (t a) where
@@ -65,3 +73,53 @@ instance (BT t, Show a) => Show (t a) where
       where
         show' leaf@(toTree -> Leaf) = show leaf
         show' tree = "(" ++ show tree ++ ")"
+
+--------------------------------------------------------------------------------
+
+class BT t => BST t where
+    node :: t a -> a -> t a -> t a
+    leaf :: t a
+
+instance BST UTree where
+    node = UNode
+    leaf = ULeaf
+
+instance BST Unbalanced where
+    node l x r = Unbalanced $ Node l x r
+    leaf = Unbalanced Leaf
+
+class Set s where
+    empty  :: s a
+    search :: Ord a => a -> s a -> Maybe a
+    insert :: Ord a => a -> s a -> s a
+    delMax :: Ord a => s a -> Maybe (a, s a)
+    delete :: Ord a => a -> s a -> s a
+
+--------------------------------------------------------------------------------
+
+-- Zadanie 6
+
+instance BST s => Set s where
+    empty  = leaf
+    search = searchBT
+    
+    insert a (toTree -> Leaf) = node leaf a leaf
+    insert a tree@(toTree -> Node l v r)
+        | a < v     = node (insert a l) v r
+        | a > v     = node l v (insert a r)
+        | otherwise = tree
+
+    delMax (toTree -> Leaf) = Nothing
+    delMax (toTree -> Node l v (toTree -> Leaf)) = Just (v, l)
+    delMax (toTree -> Node l v r) = do
+        (max, r') <- delMax r
+        return (max, node l v r')
+
+    delete a (toTree -> Leaf) = leaf
+    delete a (toTree -> Node l v r)
+        | a < v     = node (delete a l) v r
+        | a > v     = node l v (delete a r)
+        | otherwise = case delMax l of
+            Just (max, l) -> node l max r
+            Nothing       -> r
+
