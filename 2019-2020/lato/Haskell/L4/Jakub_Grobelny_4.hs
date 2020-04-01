55c661e95c257fd3f823e7d5d66683de92b3e009
Jakub Grobelny <jgrobelny1@gmail.com>
2020-04-01 20:40:30 +0200

    Haskell L4Z1

--- 2019-2020/lato/Haskell/L4/Jakub_Grobelny_4.hs
+++ 2019-2020/lato/Haskell/L4/Jakub_Grobelny_4.hs
@@ -2,3 +2,25 @@
 -- Kurs języka Haskell
 -- Lista 4, 3.04.2020
 
+-- Zadanie 1
+
+data BTree a
+    = BNode (BTree a) a (BTree a)
+    | BLeaf
+  deriving Show
+
+flatten :: BTree a -> [a]
+flatten = flatten' []
+  where
+    flatten' acc BLeaf = acc
+    flatten' acc (BNode l v r) = flatten' (v:rs) l
+      where
+        rs = flatten' acc r
+
+qsort :: Ord a => [a] -> [a]
+qsort = qsort' []
+  where
+    qsort' acc []  = acc
+    qsort' acc (pivot:xs) = qsort' (pivot:rs) [x | x <- xs, x < pivot]
+      where
+        rs = qsort' acc [x | x <- xs, x >= pivot]
