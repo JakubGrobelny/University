34781d7496ebdb430948e9a997f7ba4c98a92a37
Jakub Grobelny <jgrobelny1@gmail.com>
2020-04-01 23:23:22 +0200

    Haskell L4Z2

--- 2019-2020/lato/Haskell/L4/Jakub_Grobelny_4.hs
+++ 2019-2020/lato/Haskell/L4/Jakub_Grobelny_4.hs
@@ -2,6 +2,8 @@
 -- Kurs języka Haskell
 -- Lista 4, 3.04.2020
 
+import Control.Monad (forM)
+
 -- Zadanie 1
 
 data BTree a
@@ -24,3 +26,20 @@ qsort = qsort' []
     qsort' acc (pivot:xs) = qsort' (pivot:rs) [x | x <- xs, x < pivot]
       where
         rs = qsort' acc [x | x <- xs, x >= pivot]
+
+-- Zadanie 2
+
+queens :: Int -> [[Int]]
+queens maxN = queens' maxN
+  where
+    queens' 0 = [[]]
+    queens' n = do
+        qs <- queens' (n - 1)
+        q  <- [1 .. maxN]
+        if safe q qs 1
+            then return (q:qs)
+            else []
+    safe _ [] _ = True
+    safe q' (q:qs) col = q' /= q && 
+                         abs (q - q') /= col && 
+                         safe q' qs (col + 1)
