fa75b69c3ca6478ae1a6988196c3cc8b1ce40a39
Jakub Grobelny <jgrobelny1@gmail.com>
2020-04-03 02:34:14 +0200

    Haskell L4Z3

--- 2019-2020/lato/Haskell/L4/Jakub_Grobelny_4.hs
+++ 2019-2020/lato/Haskell/L4/Jakub_Grobelny_4.hs
@@ -44,3 +44,30 @@ queens maxN = queens' maxN
                       && abs (q - q') /= col
                       && safe q' qs (col + 1)
 
+-- Zadanie 3    
+
+data BinTree
+    = BinTreeLeaf
+    | BinTree :/\: BinTree
+  deriving Show
+
+binTree :: Int -> BinTree
+binTree 0 = BinTreeLeaf
+binTree n
+    -- sprawdzamy, czy n jest parzyste. Jeżeli jest, to wówczcas create2
+    -- nie utworzy zbędnego drzewa i wystarczy zlączyć wyniki.
+    | even n    = uncurry (:/\:) $ create2 ((n - 1) `div` 2)
+    -- jeżeli n było nieparzyste, to wówczas jedno z drzew zwracanych z create2
+    -- byłoby nieużytkiem. Wystarczy więc zamiast tego policzyć jedno drzewo
+    -- o rozmiarze (n `div` 2) i użyć go do zbudowania drzewa o rozmiarze n
+    | otherwise = let t = binTree (n `div` 2) in t :/\: t
+  where
+    even = (== 0) . (`mod` 2)
+    create2 :: Int -> (BinTree, BinTree)
+    create2 0 = (BinTreeLeaf, BinTreeLeaf :/\: BinTreeLeaf)
+    create2 m
+        | even m    = (t1 :/\: t2, t2 :/\: t2)
+        | otherwise = (t1 :/\: t1, t1 :/\: t2)
+      where
+        (t1, t2) = create2 $ (m - 1) `div` 2
+
