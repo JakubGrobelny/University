f39cf0c9aec1d00a978377f3c7d1c2f90ff0d380
Jakub Grobelny <jgrobelny1@gmail.com>
2020-04-10 18:53:10 +0200

    Haskell L5Z6

--- 2019-2020/lato/Haskell/L5/Lista_5.hs
+++ 2019-2020/lato/Haskell/L5/Lista_5.hs
@@ -3,15 +3,21 @@ Module      : Lista_5.hs
 Copyright   : Jakub Grobelny
 -}
 
+-------------------------------------------------------------------------------- 
+
 {-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE IncoherentInstances #-}
 {-# LANGUAGE MonoLocalBinds #-}
+{-# LANGUAGE ViewPatterns #-}
 
 module Lista_5 where
 
 import Control.Monad
+import Control.Applicative
+
+-------------------------------------------------------------------------------- 
 
 -- Zadanie 1
 
@@ -34,8 +40,10 @@ deepseq !a !b = rnf a `seq` b
 ($!!) :: NFData a => (a -> b) -> a -> b
 ($!!) !f !x = x `deepseq` f $! x
 
+-------------------------------------------------------------------------------- 
 
 -- Zadanie 2
+
 subseqM :: MonadPlus m => [a] -> m [a]
 subseqM [] = return mzero
 subseqM (x:xs) = do
@@ -66,5 +74,79 @@ spermM xs = do
         (y, ys) <- select xs
         return (y, x:ys)
 
+-------------------------------------------------------------------------------- 
+
 -- Zadanie 6
 
+data List t a
+    = Cons a (t a)
+    | Nil
+
+newtype SimpleList a = SimpleList { fromSimpleList :: List SimpleList a }
+
+class ListView t where
+    viewList :: t a -> List t a
+    toList :: t a -> [a]
+    cons :: a -> t a -> t a
+    nil :: t a
+    toList (viewList -> Cons x xs) = x : toList xs
+    toList (viewList -> Nil) = []
+
+data CList a 
+    = CList a :++: CList a
+    | CSingle a
+    | CNil
+
+instance ListView CList where
+    viewList (CSingle a) = Cons a CNil
+    viewList (CNil) = Nil
+    viewList (CNil :++: ys) = viewList ys
+    viewList (CSingle a :++: ys) = Cons a ys
+    viewList (xs :++: ys :++: zs) = 
+        case viewList xs of
+            Nil -> viewList $ ys :++: zs
+            Cons x xs' -> Cons x (xs' :++: ys :++: zs)
+    cons x xs = CSingle x :++: xs
+    nil = CNil
+
+instance Functor CList where
+    fmap f CNil = CNil
+    fmap f (CSingle a) = CSingle $ f a
+    fmap f (xs :++: ys) = fmap f xs :++: fmap f ys
+
+instance Applicative CList where
+    pure x = CSingle x
+    CSingle f    <*> xs = fmap f xs
+    CNil         <*> xs = CNil
+    (xs :++: ys) <*> zs = (xs <*> zs) :++: (ys <*> zs)
+
+instance Monad CList where
+    return = pure
+    CNil         >>= _ = CNil
+    (CSingle x)  >>= f = f x
+    (xs :++: ys) >>= f = (xs >>= f) :++: (ys >>= f)
+
+instance Alternative CList where
+    empty = CNil
+    (<|>) = (:++:)
+
+instance MonadPlus CList where
+    mzero = CNil
+    mplus = (:++:)
+
+instance Foldable CList where
+    foldr _ n CNil = n
+    foldr f n (CSingle x) = f x n
+    foldr f n (xs :++: ys) = foldr f (foldr f n ys) xs
+
+instance Traversable CList where
+    traverse f CNil = pure CNil
+    traverse f (CSingle x) = CSingle <$> f x
+    traverse f (xs :++: ys) = pure (:++:) <*> traverse f xs <*> traverse f ys
+
+fromList :: [a] -> CList a
+fromList [] = CNil
+fromList (x:xs) = CSingle x :++: fromList xs
+
+
+-------------------------------------------------------------------------------- 
